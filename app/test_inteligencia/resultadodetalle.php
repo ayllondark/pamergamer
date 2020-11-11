1c33c5d9fdab763228556ead773145144d321d87
paco <yuripako@gmail.com>
2020-11-11 09:45:00 -0500

    RESULTADO DETALLE

---
+++ app/test_inteligencia/resultadodetalle.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+   <style>
+   
+   body {
+  background: linear-gradient(to bottom, #8c7a7a 0%, #af877c 65%, #af877c 100%) fixed;
+  background: url("https://i.pinimg.com/originals/2d/e9/f6/2de9f63b5492b7d813728eb7d71c4279.jpg") no-repeat center center fixed!important;
+  background-size: cover!important;
+  
+  color: #d5d5d5;
+
+}
+
+   </style>
+  
+  </head>
+  <body>
+ 
+
+     <div class="container">
+         
+        
+        <div class="row" style="padding-top: 10px;">
+                <div class="col-md-3"></div>
+                <div class="col-md-6" style="text-align: center; font-size: 20px; padding: 20px;">
+                Test de Inteligencia Múltiple
+                </div>
+                <div class="col-md-3"></div>
+        </div>
+        
+        <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6" style="text-align: center; font-size: 25px; padding: 20px;">
+                Inteligencia Lingüística
+                </div>
+                <div class="col-md-3"></div>
+        </div>
+        <div class="row">
+                <div class="col-md-5"></div>
+                <div class="col-md-2">
+                    <img width="100%" src="https://image.flaticon.com/icons/png/512/260/260765.png" class="img-responsive" alt="Image"> 
+                </div>
+                <div class="col-md-5"></div>
+        </div>
+  
+     
+        <div class="row" style="padding-top: 30px; padding-bottom: 20px; ">
+                <div class="col-md-3"></div>
+              
+                <div class="col-md-3">
+                        <div class="row">
+                            <div class="col-md-3"><img width="100%" src="https://image.flaticon.com/icons/png/128/3588/3588230.png" class="img-responsive"> </div>
+                            <div class="col-md-9" style="margin-top: 13px;">Regresar</div>
+                        </div>
+
+                </div>
+               
+                <div class="col-md-3" >
+                        <div class="row">
+                          <div class="col-md-3"></div>
+                            <div class="col-md-3"><img width="100%" src="https://pngimage.net/wp-content/uploads/2018/05/cetak-png-6.png" class="img-responsive"> </div>
+                            <div class="col-md-6" style="margin-top: 13px;">Imprimir infografía</div>
+                        </div>
+                </div>
+                <div class="col-md-3"></div>
+        </div>
+
+        <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+
+                                <a class="btn btn-primary btn-block" style=" font-size: 16px;margin-bottom: 10px;border:2px solid #2c363d; ;    font-family: system-ui;font-weight: bold; text-align: left;background-color: #34414a;color: #d5d5d5;" role="button" data-toggle="collapse" href="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
+                                      Características
+                                </a>
+                                <div class="collapse" id="collapseExample1" >
+                                <div class="well" style="color: #444;">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam et fugiat velit soluta quia a cum in, porro corrupti facilis voluptates tempore recusandae pariatur iste libero minus repellendus distinctio accusantium.
+                                </div>
+                                </div>
+
+                                <a class="btn btn-primary btn-block" style="font-size: 16px; margin-bottom: 10px;border:2px solid #2c363d; ;    font-family: system-ui;font-weight: bold; text-align: left;background-color: #34414a;color: #d5d5d5;" role="button" data-toggle="collapse" href="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
+                                      Destaca en...
+                                </a>
+                                <div class="collapse" id="collapseExample2" >
+                                <div class="well" style="color: #444;">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam et fugiat velit soluta quia a cum in, porro corrupti facilis voluptates tempore recusandae pariatur iste libero minus repellendus distinctio accusantium.
+                                </div>
+                                </div>
+
+                                <a class="btn btn-primary btn-block" style="font-size: 16px;margin-bottom: 10px; border:2px solid #2c363d; ;    font-family: system-ui;font-weight: bold; text-align: left;background-color: #34414a;color: #d5d5d5;" role="button" data-toggle="collapse" href="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
+                                      Le gusta...
+                                </a>
+                                <div class="collapse" id="collapseExample3" >
+                                <div class="well" style="color: #444;">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam et fugiat velit soluta quia a cum in, porro corrupti facilis voluptates tempore recusandae pariatur iste libero minus repellendus distinctio accusantium.
+                                </div>
+                                </div>
+
+                                <a class="btn btn-primary btn-block" style="font-size: 16px;margin-bottom: 10px;border:2px solid #2c363d; ;    font-family: system-ui;font-weight: bold; text-align: left;background-color: #34414a;color: #d5d5d5;" role="button" data-toggle="collapse" href="#collapseExample4" aria-expanded="false" aria-controls="collapseExample">
+                                      Recomendaciones
+                                </a>
+                                <div class="collapse" id="collapseExample4" >
+                                <div class="well" style="color: #444;">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam et fugiat velit soluta quia a cum in, porro corrupti facilis voluptates tempore recusandae pariatur iste libero minus repellendus distinctio accusantium.
+                                </div>
+                                </div>
+                </div>
+                <div class="col-md-3"></div>
+        </div>
+
+     </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  </body>
+</html>
